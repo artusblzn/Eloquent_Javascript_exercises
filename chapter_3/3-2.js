d17c97a4ac4a528d6853782300f626945a70c771
Artus Bolzanni <artusb@tutanota.com>
2023-11-06 07:37:42 -0300

    Chapter 7 solutions added.

--- chapter_3/3-2.js
+++ chapter_3/3-2.js
@@ -10,8 +10,7 @@ another way to define whether a positive whole number is even or odd:
 • For any other number N, its evenness is the same as N - 2.
 
 Define a recursive function isEven corresponding to this description. The function
- should accept a single parameter (a positive, whole number) and return
-a Boolean.
+ should accept a single parameter (a positive, whole number) and return a Boolean.
 
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
 */
